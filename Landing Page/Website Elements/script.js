c83a16da01c48574c08e43e973ca623f331b845f
oscaralfrm <oscaralfrm1996@gmail.com>
2022-09-29 22:18:23 -0300

    Actualización de la Landing Page, ahora con cortes celular y tablet.

--- Landing Page/Website Elements/script.js
+++ Landing Page/Website Elements/script.js
@@ -1,2 +1,18 @@
-const botonOscuro = document.getElementsByClassName('modo_oscuro');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav_menu');
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll('nav_item').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}))
+
+const botonCafe = document.querySelector('.boton');
+
+botonCafe.addEventListener("click", () => {
+  window.open("https://goo.gl/maps/r45nhii8E3GZHZkj6");
+})
