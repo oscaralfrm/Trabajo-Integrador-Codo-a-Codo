c33a2c79fea76b0c8e809dabaddc1df07f4a9177
oscaralfrm <oscaralfrm1996@gmail.com>
2022-10-30 19:56:21 -0300

    Se sube validación de formulario Java Café 2

--- Java Cafe2/main.js
+++ Java Cafe2/main.js
@@ -4,37 +4,49 @@ const email= document.getElementById('email');
 const telefono= document.getElementById('telefono');
 const form = document.getElementById('form');
 const parrafo = document.getElementById('warnings');
+const patrones = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
 
 form.addEventListener('submit', e => {
+    e.preventDefault();
     let warnings = '';
     let entrar = false;
 
     parrafo.innerHTML = '';
 
-    if(nombre.value.length < 6){
+    if(nombre.value.length < 3){
         warnings += `El nombre no es valido <br>`;
         entrar = true;
+    } else {
+        warnings += `El apellido es válido 😊 <br>`;
     }
 
-    if(apellido.value.length < 6){
+    if(apellido.value.length < 3){
         warnings += `El apellido no es valido <br>`;
         entrar = true;
+    } else {
+        warnings += `El apellido es válido 😊 <br>`;
     }
 
-    if(email.value.length < 6){
+    if(email.value.length < 6 || !email.matches(patrones)){
         warnings += `El email no es valido <br>`;
         entrar = true;
+    } else {
+        warnings += `El apellido es válido 😊 <br>`;
     }
 
     if(telefono.value.length < 6){
         warnings += `El telefono no es valido <br>`;
         entrar = true;
+    } else {
+        warnings += `El apellido es válido 😊 <br>`;
     }
 
     if(entrar){
         e.preventDefault();
         parrafo.innerHTML = warnings;
     }
+
 });
 
 
